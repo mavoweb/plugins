186778ee4e4fc3a3f64cf4a22e4e07695c00208d
Lea Verou <lea@verou.me>
2017-04-11 17:28:08 -0400

    Syntax highlighting on readmes, fix Github buttons

--- view/view.js
+++ view/view.js
@@ -1,14 +1,15 @@
-(function(){
+(function() {
 
 var readme = $('[property="readme"]');
 
-if (readme) {
-	readme.addEventListener("mavo:datachange", function(evt) {
-		$$("code", readme).forEach(function(element) {
-			Prism.highlightElement(element);
-		});
+$.events(readme, "mavo:datachange", function(evt) {
+	$$("code", readme).forEach(function(element) {
+		Prism.highlightElement(element);
 	});
-}
+});
 
+$.events($('[mv-app="plugin"]'), "mavo:load", function(evt) {
+	$.include("https://buttons.github.io/buttons.js");
+});
 
 })();
