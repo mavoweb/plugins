b41aad53d313791aac2fab1d3910fc581fbe191f
Lea Verou <lea@verou.me>
2018-07-02 07:23:42 +1000

    Prevent error when env.markdown is null, store widgets load promise

--- twitter/mavo-twitter.js
+++ twitter/mavo-twitter.js
@@ -5,14 +5,16 @@ var TwitterWidgets = $.include("https://platform.twitter.com/widgets.js");
 Mavo.Plugins.register("twitter", { widgets: TwitterWidgets });
 
 Mavo.hooks.add("markdown-render-before", function(env) {
-	env.markdown = env.markdown.replace(/^\s*https:\/\/twitter.com\/\w{1,50}\/status\/\d+\s*$/gim, function(url) {
-		return `<blockquote class="twitter-tweet"><a href="${url}">${url}</a></blockquote>`;
-	});
+	if (env.markdown) {
+		env.markdown = env.markdown.replace(/^\s*https:\/\/twitter.com\/\w{1,50}\/status\/\d+\s*$/gim, function(url) {
+			return `<blockquote class="twitter-tweet"><a href="${url}">${url}</a></blockquote>`;
+		});
+	}
 });
 
 document.addEventListener("mv-markdown-render", function(evt) {
 	TwitterWidgets.then(function() {
-		twttr.widgets.load(evt.target);
+		Mavo.Plugins.loaded.twitter.rendered = twttr.widgets.load(evt.target);
 	});
 });
 
